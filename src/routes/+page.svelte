35630acae447d1c351bcaeb861f7b97e70bef25e
William Faircloth <willuhmjs@gmail.com>
2024-01-08 09:47:13 -0500

    site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>William Faircloth</h1>
+<h2>@willuhmjs</h2>
+<h3>Full-Stack Developer and Computer Scientist</h3>
+<ul>
+    <li>GitHub</li>
+    <li>LinkedIn</li>
+    <li>Instagram</li>
+    <li>YouTube</li>
+</ul>
+
+<p>I've been fascinated by computer science since primary school. I frequently challenge myself with passion projects to sharpen my expertise in the field. I contribute frequently to public repositories, and I am passionate about open-source software. Check out some of my projects below or on my GitHub page!</p>
