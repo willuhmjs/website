b4142a40dec1a6913b490f4f5eb0139e1b0b33f0
William Faircloth <willuhmjs@gmail.com>
2024-04-29 14:04:22 +0000

    use arial instead of inter, change costs, remove unneeded, etc etc

--- src/consts.ts
+++ src/consts.ts
@@ -2,10 +2,9 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "willuhmjs",
-  EMAIL: "willuhmjs",
+  EMAIL: "willuhmjs@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+
 };
 
 export const HOME: Metadata = {
@@ -30,15 +29,11 @@ export const CONTACT: Metadata = {
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/willuhmjs"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/williamfaircloth",
   }
 ];
